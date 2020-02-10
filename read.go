78c4abecc417c47a832a023198ae252feb428100
unknown <sudharahul2010@gmail.com>
2020-02-10 19:52:10 +0530

    read.go

--- read.go
+++ read.go
@@ -1,68 +1,61 @@
-// Thi is the text file format the given text file name is :name.txt
-// rahul vemuri
-// aparna vemuri
-// akanksha upasani
-// srushti shahi
-
 package main
-import ( 
+
+import (
 	"fmt"
-	"strings"
-	"os"
 	"io/ioutil"
+	"os"
+	"strings"
 )
-type name struct{
+
+type name struct {
 	fname string
 	lname string
 }
-func main(){
-	
+
+func main() {
+
 	var a name
-	
-	sli:=make([]map[string]string,0)
+
+	sli := make([]map[string]string, 0)
 
 	f, err := os.Open("name.txt")
 	fmt.Println(err)
-	
+
 	rawBytes, err := ioutil.ReadAll(f)
-	
+
 	fmt.Println(err)
 	lines := strings.Split(string(rawBytes), "\n")
-	for  i,line := range lines{
-		k:=0
-		fmt.Printf("%d",i)
-		for j,c:= range line{
-			fmt.Printf("%d",j)
-			if string(c)==" "{
-				k=1
+	for i, line := range lines {
+		k := 0
+		fmt.Printf("%d", i)
+		for j, c := range line {
+			fmt.Printf("%d", j)
+			if string(c) == " " {
+				k = 1
 				continue
 			}
 			fmt.Println(k)
-			if string(c)!=" " && k!=1{
-				a.fname=a.fname+string(c)
+			if string(c) != " " && k != 1 {
+				a.fname = a.fname + string(c)
 			}
-			if string(c)!=" " && k==1{
-				
-				a.lname=a.lname+string(c)
-				
-				
+			if string(c) != " " && k == 1 {
+
+				a.lname = a.lname + string(c)
+
 			}
-			
-			
-			
+
 		}
-		names:=map[string]string{
-			"first_name":a.fname,
-			"last_name":a.lname}
-		sli=append(sli,names)
-		
-		a.fname=" "
-		a.lname=" "
-		
+		names := map[string]string{
+			"first_name": a.fname,
+			"last_name":  a.lname}
+		sli = append(sli, names)
+
+		a.fname = " "
+		a.lname = " "
+
 	}
-	for i,line:=range sli{
-		fmt.Println(i,line)
+	for i, line := range sli {
+		fmt.Println(i, line)
 	}
 
-	
 }
